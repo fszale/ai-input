468c02adbc21ee655a2f5ba9edf9db5fee579287
Filip Szalewicz <fszalewicz@360ofme.com>
2016-12-17 17:19:30 -0500

    feelings modifications

---
+++ src/com/ai/input/InputFromKeyboard.java
@@ -0,0 +1,32 @@
+package com.ai.input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputFromKeyboard {
+
+    public static String readInputLine(String prompt)
+    {
+
+        String inString = "";
+        try
+        {
+            // create buffered reader instance
+            BufferedReader ibr = new BufferedReader( new InputStreamReader(System.in));
+            while (inString.equals(""))
+            {
+                System.out.print(prompt);
+                inString = ibr.readLine();
+            }
+
+        }
+
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        return inString;
+    }
+
+}
